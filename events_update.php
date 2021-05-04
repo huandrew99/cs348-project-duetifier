0c4a7c36afff9ee5ba247e201c0c13f9f59d1e68
grammist <75149833+grammist@users.noreply.github.com>
2021-05-04 00:08:18 -0400

    Update events_update.php

--- events_update.php
+++ events_update.php
@@ -1,6 +1,7 @@
 <?php
 
 require("assets/functions/config.php");
+mysqli_query($connection, "SET SESSION TRANSACTION ISOLATION LEVEL SERIALIZABLE");//JBZ
 
 /* Values received via ajax */
 $id = $_POST['id'];
